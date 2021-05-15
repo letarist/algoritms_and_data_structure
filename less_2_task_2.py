c587be648e3c16ebb23c23b6807a914d826e5b4a
Dmitriy <pentegov_92@mail.ru>
2021-05-15 18:40:31 +0300

    fix documentation

--- less_2_task_2.py
+++ less_2_task_2.py
@@ -1,3 +1,7 @@
+"""
+ссылка на google disk: https://drive.google.com/file/d/1P-_T3PLPUCDOewP1PfYWAjEa2fZ8WE_T/view?usp=sharing
+Посчитать четные и нечетные цифры введенного натурального числа.
+ Например, если введено число 34560, в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5)."""
 digit = int(input('Введите число: '))
 ch = 0
 nch = 0
